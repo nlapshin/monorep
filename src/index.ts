0ae05d1a312382efb554ffd882dbd4cc3b4d1210
Nikolay Lapshin <nlapshin1989@gmail.com>
2021-03-20 21:29:53 +0300

    Added read and parse package.json from file

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
-import { Command } from 'commander';
+import fs from 'fs';
 
-import pjson from '../package.json';
+import { Command } from 'commander';
+import { PackageJson } from 'type-fest'
 
 import {
   registerAddPackageCommand
 } from './commands';
 
 (() => {
+  const pjson = packageJson();
   const program = new Command();
 
-  program.version(pjson.version);
+  program.version(pjson.version || '1.0.0');
 
   registerAddPackageCommand(program);
 
   program.parse(process.argv);
 })();
+
+function packageJson(): PackageJson {
+  return JSON.parse(fs.readFileSync('package.json', 'utf-8'))
+}
+
 
