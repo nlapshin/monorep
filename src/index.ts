c5ffd1bb5600483cc9852e113218de04ea2b1209
Nikolay Lapshin <nlapshin1989@gmail.com>
2021-03-20 19:33:15 +0300

    Added parsing version from root package.json

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('test');
+import { Command } from 'commander';
+
+import pjson from '../package.json';
+
+(() => {
+  const program = new Command();
+
+  program.version(pjson.version);
+
+  program.parse(process.argv);
+})();
+
